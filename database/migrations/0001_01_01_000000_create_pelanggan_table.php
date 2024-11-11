66aad9777cd4f3f015b03fce785bf233e235d1fe
SulthanSyahrul <Sulthansyahrul2@gmail.com>
2024-11-11 10:47:47 +0700

    modified:   database/migrations/0001_01_01_000000_create_pelanggan_table.php deleted:    database/migrations/0001_01_01_000000_create_users_table.php modified:   database/migrations/0001_01_01_000002_create_jobs_table.php

--- database/migrations/0001_01_01_000000_create_pelanggan_table.php
+++ database/migrations/0001_01_01_000000_create_pelanggan_table.php
@@ -20,12 +20,6 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
     }
 
     /**
@@ -34,6 +28,5 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
     }
 };
